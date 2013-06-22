1b8f8111fae4d9bcd7c4b67523c904b99407ac1a
Sam Halliday <sam.halliday@gmail.com>
2013-06-22 22:26:38 +0100

    missing files from last commit

--- generator/src/main/java/org/netlib/GenerateJava.java
+++ generator/src/main/java/org/netlib/GenerateJava.java
@@ -2,7 +2,7 @@ package org.netlib;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroupFile;
@@ -10,11 +10,11 @@ import org.stringtemplate.v4.STGroupFile;
 /**
  * @author Sam Halliday
  */
-@Mojo(name = "java")
+@Mojo(name = "java", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GenerateJava extends AbstractMojo {
 
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException {
         STGroupFile templates = new STGroupFile("netlib-java.stg");
 
 //        File generated = new File(outputDirectory, "testing.java");
