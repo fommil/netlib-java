9bd4e8e0b40c915ed825f62c86e170e96fbe55da
Sam Halliday <sam.halliday@gmail.com>
2013-06-23 21:31:51 +0100

    interface and f2j implementation mostly done

---
+++ generator/src/main/java/org/netlib/AbstractJavaGenerator.java
@@ -0,0 +1,52 @@
+package org.netlib;
+
+import com.google.common.collect.Lists;
+import org.stringtemplate.v4.STGroupFile;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public abstract class AbstractJavaGenerator extends AbstractNetlibGenerator {
+
+    protected final STGroupFile jTemplates = new STGroupFile("netlib-java.stg", '$', '$');
+
+    protected String getTargetPackage() {
+        return outputName.replace("/", ".").substring(0, outputName.lastIndexOf("/"));
+    }
+
+    protected String getTargetClassName() {
+        return outputName.replace(".java", "").substring(outputName.lastIndexOf("/") + 1);
+    }
+
+    /**
+     * @param method
+     * @return a list of relevant parameters for the netlib interface as "fqn name"
+     */
+    protected List<String> getNetlibJavaParameters(Method method) {
+        final List<String> params = Lists.newArrayList();
+        iterateRelevantParameters(method, new ParameterCallback() {
+            @Override
+            public void process(int i, Class<?> param, String name) {
+                params.add(param.getCanonicalName() + " " + name);
+            }
+        });
+        return params;
+    }
+
+    /**
+     * @param method
+     * @return
+     */
+    protected List<String> getF2jJavaParameters(Method method) {
+        final List<String> args = Lists.newArrayList();
+        iterateRelevantParameters(method, new ParameterCallback() {
+            @Override
+            public void process(int i, Class<?> param, String name) {
+                args.add(name);
+                if (param.isArray()) args.add("0");
+            }
+        });
+        return args;
+    }
+
+}
