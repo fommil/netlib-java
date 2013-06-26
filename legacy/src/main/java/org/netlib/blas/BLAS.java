96ef663e413d312d94dbf3a942263b4c0cbe3f92
Sam Halliday <sam.halliday@gmail.com>
2013-06-26 22:38:42 +0100

    added legacy module to retain old API and groupId

---
+++ legacy/src/main/java/org/netlib/blas/BLAS.java
@@ -0,0 +1,27 @@
+package org.netlib.blas;
+
+import lombok.AccessLevel;
+import lombok.Delegate;
+import lombok.NoArgsConstructor;
+
+/**
+ * @deprecated use {@link com.github.fommil.netlib.BLAS}
+ */
+@Deprecated
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BLAS extends com.github.fommil.netlib.BLAS {
+
+    @Delegate
+    private static final com.github.fommil.netlib.BLAS DELEGATE = com.github.fommil.netlib.BLAS.getInstance();
+
+    private static final BLAS INSTANCE = new BLAS();
+
+    /**
+     * @return
+     * @deprecated use {@link com.github.fommil.netlib.BLAS#getInstance()}
+     */
+    @Deprecated
+    public static BLAS getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ legacy/src/main/java/org/netlib/lapack/LAPACK.java
@@ -0,0 +1,27 @@
+package org.netlib.lapack;
+
+import lombok.AccessLevel;
+import lombok.Delegate;
+import lombok.NoArgsConstructor;
+
+/**
+ * @deprecated use {@link com.github.fommil.netlib.LAPACK}
+ */
+@Deprecated
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class LAPACK extends com.github.fommil.netlib.LAPACK {
+
+    @Delegate
+    private static final com.github.fommil.netlib.LAPACK DELEGATE = com.github.fommil.netlib.LAPACK.getInstance();
+
+    private static final LAPACK INSTANCE = new LAPACK();
+
+   /**
+    * @return
+    * @deprecated use {@link com.github.fommil.netlib.LAPACK#getInstance()}
+    */
+   @Deprecated
+   public static LAPACK getInstance() {
+       return INSTANCE;
+   }
+}
