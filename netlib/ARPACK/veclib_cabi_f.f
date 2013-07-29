1d4c33ac3fe5893626d8bc8614572defcca744c1
Sam Halliday <sam.halliday@gmail.com>
2013-07-29 22:27:30 +0100

    use ARPACK-NG

--- netlib/ARPACK/veclib_cabi_f.f
+++
@@ -1,9 +0,0 @@
-      real function wsdot(n, x, incx, y, incy)
-      real x(*), y(*), s
-      integer n, incx, incy
-
-      call veclib_sdot(n, x, incx, y, incy, s)
-
-      wsdot = s
-      return
-      end
