68b0b0b8d57c4df932c5ee2f1ac478708fcecad0
Sam Halliday <sam.halliday@gmail.com>
2013-07-30 23:06:52 +0100

    C skeleton is being produced

--- generator/src/main/java/com/github/fommil/netlib/generator/AbstractNetlibGenerator.java
+++ generator/src/main/java/com/github/fommil/netlib/generator/AbstractNetlibGenerator.java
@@ -2,6 +2,7 @@ package com.github.fommil.netlib.generator;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import com.thoughtworks.paranamer.*;
 import org.apache.maven.artifact.Artifact;
@@ -17,123 +18,158 @@ import java.util.List;
 
 public abstract class AbstractNetlibGenerator extends AbstractMojo {
 
-    /**
-     * Location of the generated source files.
-     */
-    @Parameter(defaultValue = "${project.build.directory}/generated-sources/netlib-java", required = true)
-    protected File outputDir;
+  /**
+   * Location of the generated source files.
+   */
+  @Parameter(defaultValue = "${project.build.directory}/generated-sources/netlib-java", required = true)
+  protected File outputDir;
 
-    @Parameter(required = true)
-    protected String outputName;
+  @Parameter(required = true)
+  protected String outputName;
 
-    /**
-     * The artifact of the jar to generate from.
-     * Note that this must be listed as a <code>dependency</code>
-     * section of the calling module, not a plugin <code>dependency</code>.
-     */
-    @Parameter(defaultValue = "net.sourceforge.f2j:arpack_combined_all:jar:0.1", required = true)
-    protected String input;
+  /**
+   * The artifact of the jar to generate from.
+   * Note that this must be listed as a <code>dependency</code>
+   * section of the calling module, not a plugin <code>dependency</code>.
+   */
+  @Parameter(defaultValue = "net.sourceforge.f2j:arpack_combined_all:jar:0.1", required = true)
+  protected String input;
 
-    /**
-     * The artifact of the javadocs to extract parameter names.
-     * Note that this must be listed as a <code>dependency</code>
-     * section of the calling module, not a plugin <code>dependency</code>.
-     */
-    @Parameter(defaultValue = "net.sourceforge.f2j:arpack_combined_all:jar:javadoc:0.1")
-    protected String javadoc;
+  /**
+   * The artifact of the javadocs to extract parameter names.
+   * Note that this must be listed as a <code>dependency</code>
+   * section of the calling module, not a plugin <code>dependency</code>.
+   */
+  @Parameter(defaultValue = "net.sourceforge.f2j:arpack_combined_all:jar:javadoc:0.1")
+  protected String javadoc;
 
-    /**
-     * The package to scan.
-     */
-    @Parameter(required = true)
-    protected String scan;
+  /**
+   * The package to scan.
+   */
+  @Parameter(required = true)
+  protected String scan;
 
-    @Component
-    protected MavenProject project;
+  @Component
+  protected MavenProject project;
 
-    protected File getFile(String artifactName) {
-        // artifactMap is a bit too simplistic
-        for (Artifact artifact : project.getArtifacts())
-            if (artifact.toString().startsWith(artifactName))
-                return artifact.getFile();
-        throw new IllegalArgumentException("could not find " + artifactName + " in " + project.getArtifacts());
+  protected File getFile(String artifactName) {
+    // artifactMap is a bit too simplistic
+    for (Artifact artifact : project.getArtifacts())
+      if (artifact.toString().startsWith(artifactName))
+        return artifact.getFile();
+    throw new IllegalArgumentException("could not find " + artifactName + " in " + project.getArtifacts());
+  }
+
+  /**
+   * Implementation specific interpretation of the parameters.
+   *
+   * @param methods obtained from a scan of F2J public static methods.
+   * @return the file contents for the generated file associated to the parameters.
+   * @throws Exception
+   */
+  abstract protected String generate(List<Method> methods) throws Exception;
+
+  protected Paranamer paranamer = new DefaultParanamer();
+
+  @Override
+  public void execute() throws MojoExecutionException {
+    try {
+      project.addCompileSourceRoot(outputDir.getAbsolutePath());
+      File output = new File(outputDir, outputName);
+      if (output.exists() && project.getFile().lastModified() < output.lastModified()) {
+        getLog().info("No changes detected, skipping: " + output);
+        return;
+      }
+
+      if (Strings.isNullOrEmpty(javadoc))
+        getLog().warn("Javadocs not attached for paranamer.");
+      else
+        paranamer = new CachingParanamer(new JavadocParanamer(getFile(javadoc)));
+
+      File jar = getFile(input);
+      JarMethodScanner scanner = new JarMethodScanner(jar);
+
+      List<Method> methods = scanner.getStaticMethods(scan);
+      String generated = generate(methods);
+
+      output.getParentFile().mkdirs();
+
+      getLog().info("Generating " + output.getAbsoluteFile());
+      Files.write(generated, output, Charsets.UTF_8);
+    } catch (Exception e) {
+      throw new MojoExecutionException("java generation", e);
+    }
+  }
+
+
+  /**
+   * @param method
+   * @return parameters names for the netlib interface.
+   */
+  protected List<String> getNetlibJavaParameterNames(Method method) {
+    final List<String> params = Lists.newArrayList();
+    iterateRelevantParameters(method, new ParameterCallback() {
+      @Override
+      public void process(int i, Class<?> param, String name) {
+        params.add(name);
+      }
+    });
+    return params;
+  }
+
+  /**
+   * @param method
+   * @return canonical parameter types for the netlib interface.
+   */
+  protected List<String> getNetlibJavaParameterTypes(Method method) {
+    final List<String> types = Lists.newArrayList();
+    iterateRelevantParameters(method, new ParameterCallback() {
+      @Override
+      public void process(int i, Class<?> param, String name) {
+        types.add(param.getCanonicalName());
+      }
+    });
+    return types;
+  }
+
+  protected interface ParameterCallback {
+    void process(int i, Class<?> param, String name);
+  }
+
+  /**
+   * Calls the callback with every parameter of the method, skipping out the offset parameter
+   * introduced by F2J for array arguments.
+   *
+   * @param method
+   * @param callback
+   */
+  protected void iterateRelevantParameters(Method method, ParameterCallback callback) {
+    if (method.getParameterTypes().length == 0)
+      return;
+
+    String[] names = new String[0];
+    try {
+      names = paranamer.lookupParameterNames(method, true);
+    } catch (ParameterNamesNotFoundException e) {
+      getLog().warn(e);
     }
 
-    /**
-     * Implementation specific interpretation of the parameters.
-     *
-     * @param methods obtained from a scan of F2J public static methods.
-     * @return the file contents for the generated file associated to the parameters.
-     * @throws Exception
-     */
-    abstract protected String generate(List<Method> methods) throws Exception;
+    Class<?> last = null;
+    for (int i = 0; i < method.getParameterTypes().length; i++) {
+      Class<?> param = method.getParameterTypes()[i];
+      if (last != null && last.isArray() && param.equals(Integer.TYPE)) {
+        last = param;
+        continue;
+      }
+      last = param;
+      String name;
+      if (names.length > 0)
+        name = names[i];
+      else
+        name = "arg" + i;
 
-    protected Paranamer paranamer = new DefaultParanamer();
-
-    @Override
-    public void execute() throws MojoExecutionException {
-        try {
-            if (Strings.isNullOrEmpty(javadoc))
-                getLog().warn("Javadocs not attached for paranamer.");
-            else
-                paranamer = new CachingParanamer(new JavadocParanamer(getFile(javadoc)));
-
-            File jar = getFile(input);
-            JarMethodScanner scanner = new JarMethodScanner(jar);
-
-            List<Method> methods = scanner.getStaticMethods(scan);
-            String generated = generate(methods);
-
-            File output = new File(outputDir, outputName);
-            output.getParentFile().mkdirs();
-
-            getLog().info("Generating " + output.getAbsoluteFile());
-            Files.write(generated, output, Charsets.UTF_8);
-
-            project.addCompileSourceRoot(outputDir.getAbsolutePath());
-        } catch (Exception e) {
-            throw new MojoExecutionException("java generation", e);
-        }
+      callback.process(i, param, name);
     }
-
-    protected interface ParameterCallback {
-        void process(int i, Class<?> param, String name);
-    }
-
-    /**
-     * Calls the callback with every parameter of the method, skipping out the offset parameter
-     * introduced by F2J for array arguments.
-     *
-     * @param method
-     * @param callback
-     */
-    protected void iterateRelevantParameters(Method method, ParameterCallback callback) {
-        if (method.getParameterTypes().length == 0)
-            return;
-
-        String[] names = new String[0];
-        try {
-            names = paranamer.lookupParameterNames(method, true);
-        } catch (ParameterNamesNotFoundException e) {
-            getLog().warn(e);
-        }
-
-        Class<?> last = null;
-        for (int i = 0; i < method.getParameterTypes().length; i++) {
-            Class<?> param = method.getParameterTypes()[i];
-            if (last != null && last.isArray() && param.equals(Integer.TYPE)) {
-                last = param;
-                continue;
-            }
-            last = param;
-            String name;
-            if (names.length > 0)
-                name = names[i];
-            else
-                name = "arg" + i;
-
-            callback.process(i, param, name);
-        }
-    }
+  }
 
 }
--- generator/src/main/java/com/github/fommil/netlib/generator/NativeImplJniGenerator.java
+++ generator/src/main/java/com/github/fommil/netlib/generator/NativeImplJniGenerator.java
@@ -1,5 +1,7 @@
 package com.github.fommil.netlib.generator;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
@@ -8,6 +10,9 @@ import org.stringtemplate.v4.STGroupFile;
 
 import java.lang.reflect.Method;
 import java.util.List;
+
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
 
 @Mojo(
     name = "native-jni",
@@ -20,8 +25,33 @@ public class NativeImplJniGenerator extends AbstractNetlibGenerator {
 
   @Override
   protected String generate(List<Method> methods) throws Exception {
-    ST t = jniTemplates.getInstanceOf("dummy");
+    ST t = jniTemplates.getInstanceOf("jni");
+
+    t.add("includes", newArrayList("<cblas.h>"));
+
+    List<String> members = Lists.newArrayList();
+    for (Method method : methods) {
+      ST f = jniTemplates.getInstanceOf("function");
+      f.add("returns", "void");
+      f.add("fqn", (method.getDeclaringClass().getCanonicalName() + "." + method.getName()).replace(".", "_"));
+      f.add("paramTypes", getNetlibCParameterTypes(method));
+      f.add("paramNames", getNetlibJavaParameterNames(method));
+      f.add("return", "");
+      members.add(f.render());
+    }
+
+    t.add("members", members);
+
     return t.render();
+  }
+
+  private List<String> getNetlibCParameterTypes(Method method) {
+    return newArrayList(transform(getNetlibJavaParameterTypes(method), new Function<String, String>() {
+      @Override
+      public String apply(String input) {
+        return input;
+      }
+    }));
   }
 
 }
