9174062c8b588a390ba29a24a897e43ab6e8a1cf
Sam Halliday <sam.halliday@gmail.com>
2013-08-03 16:59:03 +0100

    blas native backend compiles, yay!

--- netlib/JNI/netlib-jni.h
+++ netlib/JNI/netlib-jni.h
@@ -9,7 +9,7 @@ typedef jobject jdoublew;
 
 // enum CBLAS_ORDER {CblasRowMajor=101, CblasColMajor=102 };
 // this is the ordering used by F2J
-#define F2J_JNI_ORDER (enum CBLAS_ORDER) 102;
+#define F2J_JNI_ORDER (enum CBLAS_ORDER) 102
 
 // convenience methods for dealing with the CBLAS (not BLAS) specific enums
 // our API is using the Fortran-style char*[] system.
