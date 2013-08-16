2020968462d6809f03bf636223e32f52de82b63f
Sam Halliday <sam.halliday@gmail.com>
2013-08-16 21:56:37 +0100

    API and F2J implementation has offsets #6

--- generator/src/main/java/com/github/fommil/netlib/generator/F2jImplGenerator.java
+++ generator/src/main/java/com/github/fommil/netlib/generator/F2jImplGenerator.java
@@ -14,42 +14,48 @@ import java.util.List;
  * Generates the F2J implementation of a netlib-java interface for the given methods.
  */
 @Mojo(
-        name = "f2j",
-        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
-        requiresDependencyResolution = ResolutionScope.COMPILE
+    name = "f2j",
+    defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+    requiresDependencyResolution = ResolutionScope.COMPILE
 )
 public class F2jImplGenerator extends AbstractJavaGenerator {
 
-    /**
-     * The interface that we are implementing.
-     */
-    @Parameter(required = true)
-    protected String implementing;
+  /**
+   * The interface that we are implementing.
+   */
+  @Parameter(required = true)
+  protected String implementing;
 
-    @Override
-    protected String generate(List<Method> methods) throws Exception {
-        List<String> members = Lists.newArrayList();
-        for (Method method : methods) {
-            ST m = jTemplates.getInstanceOf("f2jImplMethod");
-            m.add("returns", method.getReturnType());
-            m.add("method", method.getName());
-            m.add("paramTypes", getNetlibJavaParameterTypes(method));
-            m.add("paramNames", getNetlibJavaParameterNames(method));
-            m.add("impl", method.getDeclaringClass().getCanonicalName() + "." + method.getName());
-            m.add("calls", getF2jJavaParameters(method));
-            if (method.getReturnType().equals(Void.TYPE))
-                m.add("return", "");
-            members.add(m.render());
-        }
-
-        ST t = jTemplates.getInstanceOf("implClass");
-        t.add("package", getTargetPackage());
-        t.add("name", getTargetClassName());
-        t.add("members", members);
-        t.add("docs", getGenerationSummaryJavadocs());
-        t.add("parent", implementing);
-
-        return t.render();
+  @Override
+  protected String generate(List<Method> methods) throws Exception {
+    List<String> members = Lists.newArrayList();
+    for (Method method : methods) {
+      members.add(renderMethod(method, false));
+      if (hasOffsets(method))
+        members.add(renderMethod(method, true));
     }
+
+    ST t = jTemplates.getInstanceOf("implClass");
+    t.add("package", getTargetPackage());
+    t.add("name", getTargetClassName());
+    t.add("members", members);
+    t.add("docs", getGenerationSummaryJavadocs());
+    t.add("parent", implementing);
+
+    return t.render();
+  }
+
+  private String renderMethod(Method method, boolean offsets) {
+    ST m = jTemplates.getInstanceOf("f2jImplMethod");
+    m.add("returns", method.getReturnType());
+    m.add("method", method.getName());
+    m.add("paramTypes", getNetlibJavaParameterTypes(method, offsets));
+    m.add("paramNames", getNetlibJavaParameterNames(method, offsets));
+    m.add("impl", method.getDeclaringClass().getCanonicalName() + "." + method.getName());
+    m.add("calls", getF2jJavaParameters(method, offsets));
+    if (method.getReturnType().equals(Void.TYPE))
+      m.add("return", "");
+    return m.render();
+  }
 
 }
--- generator/src/main/java/com/github/fommil/netlib/generator/JavaInterfaceGenerator.java
+++ generator/src/main/java/com/github/fommil/netlib/generator/JavaInterfaceGenerator.java
@@ -14,55 +14,61 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 @Mojo(
-        name = "interface",
-        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
-        requiresDependencyResolution = ResolutionScope.COMPILE
+    name = "interface",
+    defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+    requiresDependencyResolution = ResolutionScope.COMPILE
 )
 public class JavaInterfaceGenerator extends AbstractJavaGenerator {
 
-    /**
-     * The default implementation.
-     */
-    @Parameter(required = true)
-    protected String fallback;
+  /**
+   * The default implementation.
+   */
+  @Parameter(required = true)
+  protected String fallback;
 
-    /**
-     * Arbitrary Java code that is run after instance creation.
-     */
-    @Parameter
-    protected String initCode;
+  /**
+   * Arbitrary Java code that is run after instance creation.
+   */
+  @Parameter
+  protected String initCode;
 
-    @Override
-    protected String generate(List<Method> methods) throws Exception {
-        List<String> members = Lists.newArrayList();
-        for (Method method : methods) {
-            ST m = jTemplates.getInstanceOf("abstractMethod");
-            m.add("return", method.getReturnType());
-            if (method.getReturnType().equals(Void.TYPE))
-                m.add("returnDocs", "");
-            m.add("method", method.getName());
-            m.add("paramTypes", getNetlibJavaParameterTypes(method));
-            m.add("paramNames", getNetlibJavaParameterNames(method));
-            if (!Strings.isNullOrEmpty(javadoc))
-                m.add("docs", getJavadocs(method));
-            members.add(m.render());
-        }
-
-        ST t = jTemplates.getInstanceOf("abstractClass");
-        t.add("package", getTargetPackage());
-        t.add("name", getTargetClassName());
-        t.add("members", members);
-        t.add("docs", getGenerationSummaryJavadocs());
-        t.add("fallback", fallback);
-        if (!Strings.isNullOrEmpty(initCode))
-            t.add("initCode", initCode);
-
-        return t.render();
+  @Override
+  protected String generate(List<Method> methods) throws Exception {
+    List<String> members = Lists.newArrayList();
+    for (Method method : methods) {
+      members.add(renderMethod(method, false));
+      if (hasOffsets(method))
+        members.add(renderMethod(method, true));
     }
 
-    private String getJavadocs(Method method) throws IOException {
-        File jar = getFile(javadoc);
-        F2jJavadocExtractor extractor = new F2jJavadocExtractor(jar);
-        return extractor.getJavadocDescription(method);
-    }
+    ST t = jTemplates.getInstanceOf("abstractClass");
+    t.add("package", getTargetPackage());
+    t.add("name", getTargetClassName());
+    t.add("members", members);
+    t.add("docs", getGenerationSummaryJavadocs());
+    t.add("fallback", fallback);
+    if (!Strings.isNullOrEmpty(initCode))
+      t.add("initCode", initCode);
+
+    return t.render();
+  }
+
+  private String renderMethod(Method method, boolean offsets) throws IOException {
+    ST m = jTemplates.getInstanceOf("abstractMethod");
+    m.add("return", method.getReturnType());
+    if (method.getReturnType().equals(Void.TYPE))
+      m.add("returnDocs", "");
+    m.add("method", method.getName());
+    m.add("paramTypes", getNetlibJavaParameterTypes(method, offsets));
+    m.add("paramNames", getNetlibJavaParameterNames(method, offsets));
+    if (!Strings.isNullOrEmpty(javadoc))
+      m.add("docs", getJavadocs(method));
+    return m.render();
+  }
+
+  private String getJavadocs(Method method) throws IOException {
+    File jar = getFile(javadoc);
+    F2jJavadocExtractor extractor = new F2jJavadocExtractor(jar);
+    return extractor.getJavadocDescription(method);
+  }
 }
