492d9863f29968532abc0daecc571fd2d04301ea
Sam Halliday <sam.halliday@gmail.com>
2013-09-13 22:39:52 +0100

    cuda improvements

--- perf/src/main/c/cudawrapper.c
+++ perf/src/main/c/cudawrapper.c
@@ -16,11 +16,53 @@ double cblas_ddot (const int n, const double *x, const int incx, const double *y
 	return cublasDdot(n, x, incx, y, incy);
 }
 
+void checkStatus(char* message, cublasStatus status) {
+	printf(message);
+	printf("\n");
+	fflush(stdout);
+    if (status != CUBLAS_STATUS_SUCCESS) {
+    	fprintf (stderr, "!!!! device memory allocation error (A)\n");
+    	return EXIT_FAILURE;
+    }
+}
+
 void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
                  const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
                  const int K, const double alpha, const double  *A,
                  const int lda, const double  *B, const int ldb,
                  const double beta, double  *C, const int ldc) {
+	double *cuA, *cuB, *cuC;
+	cublasStatus status;
+
+	status = cublasInit();
+	checkStatus("init",  status);	
+	status = cublasAlloc(M * N, sizeof(double),(void**)&cuA);
+	checkStatus("A", status);
+	status = cublasAlloc(N * K, sizeof(double),(void**)&cuB);
+	checkStatus("B", status);
+	status = cublasAlloc(M * K, sizeof(double),(void**)&cuC);
+	checkStatus("C", status);
+	
+	status = cublasSetMatrix(M, N, sizeof(double), A, lda, cuA, lda);
+	checkStatus("setA", status);
+
+	status = cublasSetMatrix(M, N, sizeof(double), B, ldb, cuB, ldb);
+	checkStatus("setB", status);
+
+	// status = cublasSetMatrix(M, N, sizeof(double), C, ldc, cuC, ldc);
+	// checkStatus("setC", status);
+	
 	// HACK: ignore trans			   
-	cublasDgemm('N', 'N', M, N, K, alpha, A, lda, B, ldb, beta, C, ldc);
+	status = cublasDgemm('N', 'N', M, N, K, alpha, cuA, lda, cuB, ldb, beta, cuC, ldc);
+	checkStatus("dgemm", status);
+	
+	status = cubblasGetMatrix(M, N, sizeof(double), cuC, ldc, C, ldc);
+	checkStatus("setB", status);
+	
+	status = cublasFree(cuA);
+	checkStatus("freeA", status);
+	status = cublasFree(cuB);
+	checkStatus("freeB", status);
+	status = cublasFree(cuC);
+	checkStatus("freeC", status);
 }
